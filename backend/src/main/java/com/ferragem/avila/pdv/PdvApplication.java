58cca612af19bc64f05963362b1a107665b71f32
GabryelMelo07 <gabryelcraft@gmail.com>
2024-03-01 17:40:07 -0300

    Initial commit

---
+++ backend/src/main/java/com/ferragem/avila/pdv/PdvApplication.java
@@ -0,0 +1,13 @@
+package com.ferragem.avila.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvApplication.class, args);
+	}
+
+}
