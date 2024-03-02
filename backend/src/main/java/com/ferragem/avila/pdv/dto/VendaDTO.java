3215517eec1ea06876f6e063b7adfe273df90eb5
GabryelMelo07 <gabryelcraft@gmail.com>
2024-03-02 03:48:14 -0300

    Add Venda dto

---
+++ backend/src/main/java/com/ferragem/avila/pdv/dto/VendaDTO.java
@@ -0,0 +1,12 @@
+package com.ferragem.avila.pdv.dto;
+
+import java.time.LocalDateTime;
+
+import com.ferragem.avila.pdv.model.enums.FormaPagamento;
+
+import jakarta.validation.constraints.FutureOrPresent;
+
+public record VendaDTO(@FutureOrPresent LocalDateTime dataHora,
+        boolean concluida,
+        FormaPagamento formaPagamento) {
+}
