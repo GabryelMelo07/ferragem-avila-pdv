4891829ee795b0e16f11be9437a2f1203d97b9b3
GabryelMelo07 <gabryelcraft@gmail.com>
2024-03-06 17:02:48 -0300

    Update Models & Enums

--- backend/src/main/java/com/ferragem/avila/pdv/model/Item.java
+++ backend/src/main/java/com/ferragem/avila/pdv/model/Item.java
@@ -2,7 +2,8 @@ package com.ferragem.avila.pdv.model;
 
 import java.math.BigDecimal;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ferragem.avila.pdv.model.enums.UnidadeMedida;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,8 +13,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "item")
 public class Item {
@@ -28,12 +31,35 @@ public class Item {
     @Column
     private BigDecimal preco;
 
+    @Column
+    private BigDecimal precoUnitarioProduto;
+
     @ManyToOne
     @JoinColumn(name = "produto_id")
     private Produto produto;
 
     @ManyToOne
     @JoinColumn(name = "venda_id")
-    @JsonIgnoreProperties("itens")
+    @JsonIgnore
     private Venda venda;
+
+    public Item(Float quantidade, Produto produto) {
+        this.quantidade = quantidade;
+        this.precoUnitarioProduto = produto.getPreco();
+        this.produto = produto;
+        calcularPrecoTotal(quantidade);
+    }
+    
+    public void calcularPrecoTotal(float quantidade) {
+        if (produto.getUnidadeMedida() == UnidadeMedida.GRAMA) {
+            BigDecimal precoPorKg = produto.getPreco().divide(new BigDecimal(1000));
+            this.setPreco(precoPorKg.multiply(new BigDecimal(quantidade)));
+        } else if (produto.getUnidadeMedida() == UnidadeMedida.METRO) {
+            BigDecimal precoPorMetro = produto.getPreco().divide(new BigDecimal(100));
+            this.setPreco(precoPorMetro.multiply(new BigDecimal(quantidade)));
+        } else {
+            this.setPreco(produto.getPreco().multiply(new BigDecimal(quantidade)));
+        }
+    }
+    
 }
--- backend/src/main/java/com/ferragem/avila/pdv/model/enums/FormaPagamento.java
+++ backend/src/main/java/com/ferragem/avila/pdv/model/enums/FormaPagamento.java
@@ -1,17 +1,8 @@
 package com.ferragem.avila.pdv.model.enums;
 
-import lombok.Getter;
-
-@Getter
 public enum FormaPagamento {
-    PIX("pix"),
-    DEBITO("debito"),
-    CREDITO("credito"),
-    DINHEIRO("dinheiro");
-
-    private String formaPagamento;
-
-    FormaPagamento(String formaPagamento) {
-        this.formaPagamento = formaPagamento;
-    }
+    PIX,
+    DEBITO,
+    CREDITO,
+    DINHEIRO
 }
