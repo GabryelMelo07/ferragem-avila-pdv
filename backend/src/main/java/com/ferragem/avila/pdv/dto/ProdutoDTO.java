9c117f7e06cda8fd897784b72f07740a008903dc
GabryelMelo07 <gabryelcraft@gmail.com>
2024-03-08 01:58:24 -0300

    Update ProdutoDto

--- backend/src/main/java/com/ferragem/avila/pdv/dto/ProdutoDTO.java
+++ backend/src/main/java/com/ferragem/avila/pdv/dto/ProdutoDTO.java
@@ -12,8 +12,9 @@ import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 public record ProdutoDTO(
-        @Size(max = 70, message = "Campo descrição deve ter no máximo 70 caracteres") @NotBlank @NotEmpty String descricao,
-        UnidadeMedida unidadeMedida,
-        @PositiveOrZero Float estoque, @Positive @Max(value = 999999) BigDecimal preco,
-        @Size(max = 13, message = "Campo código de barras deve ter no máximo 13 caracteres, padrão EAN 13.") String codigoBarrasEAN13) {
+                @Size(max = 70, message = "Campo descrição deve ter no máximo 70 caracteres") @NotBlank @NotEmpty String descricao,
+                UnidadeMedida unidadeMedida,
+                @PositiveOrZero Float estoque, @Positive @Max(value = 999999) BigDecimal precoFornecedor,
+                @Positive @Max(value = 999999) BigDecimal preco,
+                @Size(max = 13, message = "Campo código de barras deve ter no máximo 13 caracteres, padrão EAN 13.") String codigoBarrasEAN13) {
 }
