a033cadaa0611f99cbffa5f53421223b001c3059
GabryelMelo07 <gabryelcraft@gmail.com>
2024-04-02 22:43:53 -0300

    Add: Cors configuration

---
+++ backend/src/main/java/com/ferragem/avila/pdv/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ferragem.avila.pdv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:3000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+    
+}
