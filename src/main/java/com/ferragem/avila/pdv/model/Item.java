ce9c08f9d9dabb0cf376579085ed5ea41d911462
GabryelMelo07 <gabryelcraft@gmail.com>
2024-09-12 00:36:49 -0300

    refactor: Adjusting jumps in item ids

--- src/main/java/com/ferragem/avila/pdv/model/Item.java
+++ src/main/java/com/ferragem/avila/pdv/model/Item.java
@@ -9,6 +9,7 @@ import com.ferragem.avila.pdv.model.enums.UnidadeMedida;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,7 +24,7 @@ import lombok.NoArgsConstructor;
 public class Item implements Serializable {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
